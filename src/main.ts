2b185497c1e2c6326c50ee6fac5bb284f5770fc9
João Matheus Lima Lúcio <joaomatheuslimalucio@gmail.com>
2021-02-12 20:23:22 -0300

    add server info

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const PORT = config.get('server.port')
+  const HOST = config.get('server.host')
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  Logger.debug(`[PORT]: ${PORT}`)
+  Logger.debug(`[Environment] ${process.env.NODE_ENV || 'development'}`)
+  Logger.debug(`[SERVER AT]: http://${HOST}:${PORT}/graphql`)
 }
 bootstrap();
