18536bf934e0975d140b2cb6ad2938bd06303dd3
Alvaro Farias <alvardev20@gmail.com>
2021-06-17 18:51:52 -0300

    correcao bugs

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ import * as config from 'config';
 async function bootstrap() {
   const PORT = config.get('server.port')
   const HOST = config.get('server.host')
+  const PROTOCOL = config.get('server.https') ? "https" : "http"
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   Logger.debug(`[PORT]: ${PORT}`)
   Logger.debug(`[Environment] ${process.env.NODE_ENV || 'development'}`)
-  Logger.debug(`[SERVER AT]: http://${HOST}:${PORT}/graphql`)
+  Logger.debug(`[SERVER AT]: ${PROTOCOL}://${HOST}:${PORT}/graphql`)
 }
 bootstrap();
